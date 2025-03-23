82d5731e2b6c4ea2623a9e75e75fd687bebf4ef6
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-03-23 16:54:00 -0400

    remove link for title

--- src/app/components/BlogCard.jsx
+++ src/app/components/BlogCard.jsx
@@ -19,78 +19,73 @@
 //post is from the parent component called BlogForm
 //onDelete is from the parent component called BlogForm
 
-"use client";
-import React, { useState } from "react";
+'use client';
+import React, { useState } from 'react';
 // console.log("data", { userName, content, title, date, fileUpload });
 export default function BlogCard({ post, onDelete, onEdit }) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   //function to expand the content of the post
   function expandButton() {
-    console.log("Expand button clicked");
+    console.log('Expand button clicked');
     //sets the state of isExpanded to the opposite of what it currently is
     setIsExpanded(!isExpanded);
   }
 
   function deleteButton() {
-    console.log("Delete button clicked");
+    console.log('Delete button clicked');
     onDelete();
   }
 
   function editButton() {
-    console.log("Edit button clicked");
+    console.log('Edit button clicked');
     onEdit();
   }
 
-  console.log("BlogCard post:", post);
+  console.log('BlogCard post:', post);
   // Check if the post object is valid
   if (!post || Object.keys(post).length === 0) {
     return null;
   }
 
   return (
-<article className="flex flex-col sm:flex-row bg-white transition hover:shadow-xl mb-3 px-6 py-6 sm:px-6 sm:py-8">
-{/* <div className="rotate-180 p-2 [writing-mode:_vertical-lr]"> */}
-<div className="p-2 sm:rotate-180 sm:[writing-mode:_vertical-lr]">
-
+    <article className="flex flex-col sm:flex-row bg-white transition hover:shadow-xl mb-3 px-6 py-6 sm:px-6 sm:py-8">
+      {/* <div className="rotate-180 p-2 [writing-mode:_vertical-lr]"> */}
+      <div className="p-2 sm:rotate-180 sm:[writing-mode:_vertical-lr]">
         <time
-          dateTime={post.date || ""}
+          dateTime={post.date || ''}
           className="flex items-center justify-between gap-4 text-xs font-bold uppercase text-amber-600"
         >
           <span>
             {post.date
               ? new Date(post.date).toLocaleDateString(undefined, {
-                  month: "short",
-                  day: "numeric",
+                  month: 'short',
+                  day: 'numeric',
                 })
-              : "N/A"}
+              : 'N/A'}
           </span>
           {/* Responsive Divider */}
           {/* <span className="w-px flex-1 bg-gray-900/10"></span> */}
           <span className="bg-gray-900/10 sm:w-px sm:flex-1 sm:h-auto w-full h-px"></span>
 
-          <span>{post.date ? new Date(post.date).getFullYear() : "N/A"}</span>
+          <span>{post.date ? new Date(post.date).getFullYear() : 'N/A'}</span>
         </time>
       </div>
       <div className="sm:block sm:basis-56">
         <img
           alt="uploaded file"
-          src={post.fileUpload || "javascript.jpg"} //FALLBACK HERE
+          src={post.fileUpload || 'javascript.jpg'} //FALLBACK HERE
           className="aspect-square h-56 w-full object-cover"
         />
       </div>
 
       <div className="flex flex-1 flex-col justify-between">
         <div className="border-s border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">
-          <a href="#">
-            <h3 className="font-bold uppercase text-amber-600">{post.title}</h3>
-          </a>
-          <div className="text-black font-light text-xs">
-            {post.userName}
-          </div>
+          <h3 className="font-bold uppercase text-amber-600">{post.title}</h3>
+          <div className="text-black font-light text-xs">{post.userName}</div>
 
           <p
-            className={`mt-2 text-sm/relaxed text-gray-700 ${!isExpanded ? "line-clamp-3" : isExpanded}`}
+            className={`mt-2 text-sm/relaxed text-gray-700 ${!isExpanded ? 'line-clamp-3' : isExpanded}`}
           >
             {post.content}
           </p>
@@ -102,7 +97,7 @@ export default function BlogCard({ post, onDelete, onEdit }) {
             onClick={expandButton}
             className=" block bg-amber-700 px-6 py-3 mx-1.5 my-1.5 text-center text-xs font-bold uppercase text-white hover:bg-amber-800 transition  rounded-sm"
           >
-            {isExpanded ? "Read Less" : "Expand"}
+            {isExpanded ? 'Read Less' : 'Expand'}
           </button>
 
           <button
--- src/app/components/New-Post.jsx
+++ src/app/components/New-Post.jsx
@@ -101,7 +101,7 @@ export default function NewPost({ onCancel, onAddPost, post }) {
                   value={title}
                   onChange={(e) => setTitle(e.target.value)}
                   rows={1}
-                  className="block rounded-md w-full bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  className="block rounded-md w-full bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
                 />
               </div>
             </div>
