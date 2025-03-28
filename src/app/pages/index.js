09830eb7598aea53af7748b62fed7b7ec82def81
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-03-27 23:05:00 -0400

    add accordion and components

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -3,12 +3,14 @@ import React from "react";
 import BlogForm from "../components/BlogForm";
 import BlogCard from "../components/BlogCard";
 import Footer from "../components/Footer";
+import AccordionCustomIcon from "../components/Accordion";
 
 export default function Index() {
   return (
     <div>
       <BlogForm />
       <BlogCard />
+      <AccordionCustomIcon />
       <Footer />
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+const withMT = require(@material-tailwind/react/utils/withMT);
+
+
+
+module.exports = withMT({
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}", // Adjust this path based on your project structure
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  });
